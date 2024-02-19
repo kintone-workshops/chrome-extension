fe3202b7b704acb60a1247f3bb3f70d5e598a94f
sean-kintone <seanluse@cybozu.onmicrosoft.com>
2024-02-19 16:21:32 +0900

    Nevermind I decided to make it a side-bar extension

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,8 +1,12 @@
 import Browser from "webextension-polyfill";
 
 //background.js
-Browser.action.onClicked.addListener((tab) => {
-  Browser.tabs.query({ active: true, currentWindow: true }).then((activeTab) => {
-    Browser.tabs.sendMessage(activeTab[0].id!, { type: "popup-modal" });
-  });
-});
+// If you want to open as a pop-up
+// Browser.action.onClicked.addListener((tab) => {
+//   Browser.tabs.query({ active: true, currentWindow: true }).then((activeTab) => {
+//     Browser.tabs.sendMessage(activeTab[0].id!, { type: "popup-modal" });
+//   });
+// });
+
+// Allows users to open the side panel by clicking on the action toolbar icon
+chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch((error) => console.error(error));
